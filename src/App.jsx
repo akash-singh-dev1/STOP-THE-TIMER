2e9eb4cda7eecd3083c10cc395e424b581280c60
Akash Singh <akashsingh.dev1@gmail.com>
2026-01-02 15:39:03 +0530

    updates in this commit.
    -- added TimerChallenge component and its styling this component has tittle and targetTime till which you want the timer and it has start and stop button for timer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Player from "./components/Player/Player";
 import Header from "./components/Header/Header";
+import TimerChallenge from "./components/TimerChallenge/TimerChallenge";
 function App() {
   return (
     <>
       <Header />
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge tittle={"easy"} targetTime={1} />
+        <TimerChallenge tittle={"not easy"} targetTime={5} />
+        <TimerChallenge tittle={"getting tough"} targetTime={10} />
+        <TimerChallenge tittle={"pros only"} targetTime={15} />
+      </div>
     </>
   );
 }
---
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import s from "./TimerChallenge.module.css";
+import { useState, useRef } from "react";
+
+const TimerChallenge = ({ tittle, targetTime }) => {
+  //state to know whether timer started or not.
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  //state to know timer is expired or not.
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  // ref to store the timerId so that we can stop it.
+  const timerId = useRef();
+
+  //function to start and stop timer.
+  const handleStart = () => {
+    timerId.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
+  //function to stop the timer.
+  const handleStop = () => {
+    clearTimeout(timerId.current);
+    setTimerStarted(false);
+    setTimerExpired(false);
+  };
+  return (
+    <section className={s.challenge}>
+      <h2>{tittle}</h2>
+      {timerExpired ? <p>YOU LOST THE CHALLENGE</p> : ""}
+      <p className={s["challenge-time"]}>{targetTime} second</p>
+      <button onClick={timerStarted ? handleStop : handleStart} type="button">
+        {timerStarted ? "stop" : "start"} challenge
+      </button>
+      <p className={timerStarted ? s.active : undefined}>
+        {timerStarted ? "Time is running" : "Timer inactive"}
+      </p>
+    </section>
+  );
+};
+export default TimerChallenge;
